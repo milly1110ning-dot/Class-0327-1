8de3dd991402bad9b4d0d11f2c52e2f73fe1f45a
milly1110ning-dot <milly.1110ning@gmail.com>
2026-03-27 09:46:35 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Test
+{
+	private int Mid;
+	private int Final;
+
+	public int MyMid { get; }=98
+	
+		/*get
+		{
+			return Mid;
+		}
+		set
+		{
+			if(value < 0 || value > 100)
+			{
+				Mid = 0;
+			}
+			else
+			{
+				Mid = value;
+			}
+		}*/
+	
+
+	public int MyFinal
+	{
+		get
+		{
+			return Final;
+		}
+		set
+		{
+			if (value > 0 || value < 100)
+			{
+				Final = value;
+			}
+			else
+			{
+				Final = 0;
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Class_0327_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std=new Test();
+
+            //std.MyMid = 92;
+            std.MyFinal = 78;
+
+            Console.WriteLine(std.MyMid.ToString());
+            Console.WriteLine(std.MyFinal.ToString());
+
+
+        }
+    }
+}
